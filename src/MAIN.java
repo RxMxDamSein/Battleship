0a3ca2f2cacb32186aff2668a422cec76bfab89c
Dennis <denniswurzbacher@gmail.com>
2020-10-09 12:30:08 +0200

    Ich hen 1 Zeile hinzugefickt

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,5 +1,7 @@
 public class MAIN {
     public static void main(String[] args) {
         System.out.println("THIS WAS WRITTEN BY ROMAN :O!!!");
+        System.out.println("DAS WURDE VON DENNIS VERFASST");
     }
+
 }
