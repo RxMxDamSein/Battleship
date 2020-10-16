265d1b04ddb6c88b98ba7bfcab0ac07688229bad
Arkanipro <Arkanipro@gmail.com>
2020-10-16 11:23:34 +0200

    Revert "JavaFX window made with this Tutorial: https://www.youtube.com/playlist?list=PL6gx4Cwl9DGBzfXLWLSYVy8EbTdpGbUIG"
    This reverts commit 6821f4919bbcd11f9282ab1157967fa501220904.

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,35 +1,9 @@
-
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-
-public class MAIN extends Application implements EventHandler<ActionEvent> {
-    Button button;
-
+public class MAIN {
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("THIS WAS WRITTEN BY ROMAN :O!!!");
+        System.out.println("DAS WURDE VON DENNIS VERFASST");
+        System.out.println("Dennis hat ne zweite Zeile gemaked");
+        System.out.println("Romasn zweite Zeile");
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("TITEL");
-        StackPane layout=new StackPane();
-        button=new Button();
-        button.setText("CLICK! ");
-        button.setOnAction(this);
-        Scene scene=new Scene(layout,420,300);
-        layout.getChildren().add(button);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    @Override
-    public void handle(ActionEvent event) {
-        button.setText(button.getText()+"pog ");
-    }
 }
