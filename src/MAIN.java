d772f865f71463563e034d75a24b931597b37679
Arkanipro <Arkanipro@gmail.com>
2020-11-06 15:26:50 +0100

    Playing against bot still very buggy xD does work a little bit ;)

--- src/MAIN.java
+++ src/MAIN.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -14,6 +15,7 @@ public class MAIN extends Application  {
     TextField inputFieldX;
     TextField inputFieldY;
     Button buttonSubmit;
+    Button buttonSubmitBot;
     public static void main(String[] args) {
         System.out.println("Working Directory = " +
                 System.getProperty("user.dir"));
@@ -31,9 +33,10 @@ public class MAIN extends Application  {
         inputFieldY=new TextField();
         inputFieldY.setPromptText("5");
         inputFieldY.setOnAction(e->submitFunc());
-        buttonSubmit=new Button("SUBMIT");
+        buttonSubmit=new Button("SUBMIT 2P");
         buttonSubmit.setOnAction(e->submitFunc());
-
+        buttonSubmitBot=new Button("BOT");
+        buttonSubmitBot.setOnAction(e->submitFuncBot());
 
         /*MenuBar menuBar=new MenuBar();
         Menu m1=new Menu("Stift!");
@@ -45,14 +48,31 @@ public class MAIN extends Application  {
         vBox.setPadding(new Insets(10,10,10,10));
         vBox.setSpacing(5);
         //vBox.getChildren().addAll(menuBar,inputFieldX,inputFieldY,buttonSubmit);
-        vBox.getChildren().addAll(inputFieldX,inputFieldY,buttonSubmit);
+        HBox hBox=new HBox(5);
+        hBox.getChildren().addAll(buttonSubmit,buttonSubmitBot);
+        vBox.getChildren().addAll(inputFieldX,inputFieldY,hBox);
 
         scene_sizeS=new Scene(vBox);
         window.setScene(scene_sizeS);
         window.show();
     }
+    private void submitFuncBot()  {
+        int[] xy=checkXY();
+        int x=xy[0];
+        int y=xy[1];
+        //System.out.println("X: "+x+" Y: "+y);
+        new BotGrid(window,x,y,scene_sizeS);
+    }
 
     private void submitFunc()  {
+        int[] xy=checkXY();
+        int x=xy[0];
+        int y=xy[1];
+        System.out.println("X: "+x+" Y: "+y);
+        new Grid(window,x,y,scene_sizeS);
+
+    }
+    private int[] checkXY(){
         String xS=inputFieldX.getText();
         String yS=inputFieldY.getText();
         int x=0,y=0;
@@ -71,9 +91,6 @@ public class MAIN extends Application  {
             if(y<=0)
                 y=5;
         }
-        System.out.println("X: "+x+" Y: "+y);
-        new Grid(window,x,y,scene_sizeS);
-
+        return new int[]{x,y};
     }
-
 }
